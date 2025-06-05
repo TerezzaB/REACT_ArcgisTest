c43622ab947601b8fa1aa86e7e92d3ee16907b38
Terézia Boďová <bodova.terezia13@gmail.com>
2025-06-05 15:54:13 +0200

    5.6. - first map changed

--- src/views/MapView1.jsx
+++ src/views/MapView1.jsx
@@ -8,21 +8,6 @@ export default function MapView1() {
         item-id="d5dda743788a4b0688fe48f43ae7beb9"
         style={{ height: "100%", width: "100%" }}
       >
-        {/* Point */}
-        <arcgis-graphic
-          geometry='{"type":"point","longitude":17.1077,"latitude":48.1486}'
-          symbol='{"type":"simple-marker","color":"red","size":14}'
-        ></arcgis-graphic>
-        {/* Line */}
-        <arcgis-graphic
-          geometry='{"type":"polyline","paths":[[[17.1,48.14],[17.12,48.15],[17.13,48.13]]]}'
-          symbol='{"type":"simple-line","color":"blue","width":3}'
-        ></arcgis-graphic>
-        {/* Polygon */}
-        <arcgis-graphic
-          geometry='{"type":"polygon","rings":[[[17.105,48.147],[17.11,48.147],[17.11,48.15],[17.105,48.15],[17.105,48.147]]]}'
-          symbol='{"type":"simple-fill","color":[0,255,0,0.3],"outline":{"color":"green","width":2}}'
-        ></arcgis-graphic>
       </arcgis-map>
     </div>
   );
